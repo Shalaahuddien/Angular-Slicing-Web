3c764156251c8e206541d8b7c6e54748d9f58d30
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-02-26 09:42:51 +0700

    Learn

--- menu/menu.component.ts
+++ menu/menu.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+  constructor(private service:OrderDetailsService) { }
+  foodData:any;
+  ngOnInit(): void {
+    this.foodData = this.service.foodDetails;
+  }
 
 }
